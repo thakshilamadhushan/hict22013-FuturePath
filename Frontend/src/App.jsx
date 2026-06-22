53817289943a071ba2db53f500acc19573029ad6
Thakshila Madhushan <thakshilamadushan900@gmail.com>
2026-06-22 14:01:04 +0530

    Create Hero Section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Navbar from './components/navBar.jsx'
+import HeroSection from './components/heroSection.jsx'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <HeroSection />
     </>
   )
 }
